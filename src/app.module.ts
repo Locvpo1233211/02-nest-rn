7d91f742671d98af8551d9bd7c924d196570c264
Locvpo1233211 <trongnghia1112223333@gmail.com>
2025-06-23 10:11:28 +0700

    tạo base models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReviewModule } from './reviews/review.module';
+import { RestaurantModule } from './restaurants/restaurant.module';
+import { OrderModule } from './orders/order.module';
+import { OrderDetailModule } from './order.detail/order.detail.module';
+import { MenusModule } from './menus/menus.module';
+import { MenuItemsModule } from './menu.items/menu.items.module';
+import { MenuItemOptionsModule } from './menu.item.options/menu.item.options.module';
+import { LikesModule } from './likes/likes.module';
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ReviewModule,
+    RestaurantModule,
+    OrderModule,
+    OrderDetailModule,
+    MenusModule,
+    MenuItemsModule,
+    MenuItemOptionsModule,
+    LikesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
